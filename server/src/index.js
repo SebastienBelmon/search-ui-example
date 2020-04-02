40827a35aaa6b853183c48336a3cc1b8f64da717
nenu-git <sebastien.belmon@gmail.com>
2020-04-02 13:29:09 +0200

    ajout commentaires

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,18 @@
+// importation des packages / middleware
 const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const { Client } = require('@elastic/elasticsearch');
 
+// initialisation des packages
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+// création d'une instance client d'elasticsearch
 const client = new Client({
   node: 'http://localhost:9200',
   auth: {
@@ -19,15 +21,23 @@ const client = new Client({
   },
 });
 
-app.get('/', async (req, res) => {
+// accepte requete http GET pour query une recherche
+// sur l'index national-parks
+app.get('/national-parks', async (req, res) => {
+  // le body de la requete doit etre un JSON, il faut donc bien
+  // penser à mettre -H "Content-Type: application/json" dans la
+  // requete cURL
   const queryResults = await client.search({
     index: 'national-parks',
     body: req.body,
   });
 
+  // on renvoie seulement les résultats de la recherche, pas de méta données
   res.send(queryResults.body.hits.hits);
 });
 
+
+// requete pour avoir des infos sur les indexes d'elasticsearch
 app.get('/indices', async (req, res) => {
   const queryResults = await client.cat.indices({
     format: "json",
@@ -37,4 +47,5 @@ app.get('/indices', async (req, res) => {
   res.send(queryResults.body);
 });
 
+// écoute serveur sur le port local 3961
 app.listen(3961, () => console.log('App running at http://localhost:3961'));
