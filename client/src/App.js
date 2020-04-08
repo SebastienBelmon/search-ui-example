4e75a0f4e90de27f400b82173ea30cb4d6b79f50
nenu-git <sebastien.belmon@gmail.com>
2020-04-08 10:52:32 +0200

    remove useless code

--- client/src/App.js
+++ client/src/App.js
@@ -37,67 +37,8 @@ const App = () => {
     <SearchProvider
       config={{
         ...connector,
-        debug: true,
-        hasA11yNotifications: true,
-        // selon https://www.elastic.co/fr/blog/how-to-build-great-react-search-experiences-quickly
-        // ajout des principales configs
-        searchQuery: {
-          search_fields: {
-            // 1. Recherche par nom de lieu
-            title: {},
-          },
-          // 2. Résultats de la recherche : title, description, id, nps_link
-          result_fields: {
-            title: {
-              // snippet = recherche qui match dans une balise <em> (surligné)
-              snippet: {
-                size: 120, // limite le nbr de caractères du snippet
-                fallback: true,
-              },
-            },
-            description: {
-              snippet: {
-                size: 200,
-                fallback: true,
-              },
-            },
-            id: {
-              snippet: {
-                size: 75,
-                fallback: true,
-              },
-            },
-            nps_link: {
-              snippet: {
-                size: 100,
-                fallback: true,
-              },
-            },
-          },
-          // 3. recherche à facette par états, si patrimoine humanité, nbr de visiteurs
-          facets: {
-            states: {
-              type: 'value',
-              size: 5,
-            },
-            world_heritage_site: {
-              type: 'value',
-              size: 5,
-            },
-            visitors: {
-              type: 'range',
-              ranges: [
-                { from: 0, to: 10000, name: 'Quasi nulle' },
-                { from: 10001, to: 100000, name: 'Très faible' },
-                { from: 100001, to: 500000, name: 'Faible' },
-                { from: 500001, to: 1000000, name: 'Moyenne' },
-                { from: 1000001, to: 5000000, name: 'Forte' },
-                { from: 5000001, to: 10000000, name: 'Très forte' },
-                { from: 10000001, name: 'Extrème' },
-              ],
-            },
-          },
-        },
+        debug: process.env.NODE_ENV !== 'production' ? true : false,
+        hasA11yNotifications: process.env.NODE_ENV !== 'production' ? true : false,
       }}
     >
       <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
